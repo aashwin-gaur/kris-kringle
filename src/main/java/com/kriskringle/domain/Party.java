d86f672b403e2dfa023dea1a8e0f8464e38f2f78
aashwin-gaur <gaur.aashwin95@gmail.com>
2017-10-09 00:04:46 +1100

    changed from h2 to mysql and added a converter that converts localDate to java.sql.date behind the scenes

--- src/main/java/com/kriskringle/domain/Party.java
+++ src/main/java/com/kriskringle/domain/Party.java
@@ -5,11 +5,8 @@ import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-
-import org.hibernate.annotations.GenericGenerator;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
